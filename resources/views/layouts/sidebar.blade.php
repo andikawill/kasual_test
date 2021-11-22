d98bcdfdecdd72aa3292871a91968ca9ee647c66
Willy andika <willyandika007@gmail.com>
2021-11-22 12:13:59 +0700

    done

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIG1UknjFmzC544zgc-usoG6mzRM989O-jHg&usqp=CAU"
              alt="{{ config('app.name') }} Logo"
              class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
